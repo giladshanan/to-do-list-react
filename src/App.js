a53d3a65d051cdafedbdd774fed983322b0b8abe
Gilad Shanan <giladshanan@gmail.com>
2017-11-02 12:00:40 -0500

    add checkboxes that toggle strikethrough

--- src/App.js
+++ src/App.js
@@ -1,6 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class ListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      complete: false
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    if (this.state.complete == false) {
+      this.refs.listli.style.setProperty("text-decoration", "line-through");
+      this.setState({
+        complete: true
+      });
+    } else {
+      this.refs.listli.style.setProperty("text-decoration", "none");
+      this.setState({
+        complete: false
+      });
+    }
+  }
+
+  render() {
+    return (
+      <li ref='listli' >
+        <input
+          name="isDone"
+          type="checkbox"
+          onChange={this.handleInputChange}
+         />
+        {this.props.value}
+      </li>
+    );
+  }
+}
+
+
+const ToDoList = props => {
+  const listItems = props.items.map(item =>
+    <ListItem key={item.slice(0, 6) + "- " + (new Date()).getTime()}
+              value={item} />
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
 class ToDoForm extends Component {
   constructor(props) {
     super(props);
@@ -20,8 +71,7 @@ class ToDoForm extends Component {
   handleSubmit(event) {
     alert('A to do list item was submitted: ' + this.state.value);
     event.preventDefault();
-    var newItem = <li key={this.state.listItems.length}>{this.state.value}</li>
-    this.setState({listItems: this.state.listItems.concat(newItem)});
+    this.setState({listItems: this.state.listItems.concat(this.state.value)});
   }
 
   render() {
@@ -34,15 +84,13 @@ class ToDoForm extends Component {
           </label>
           <input type="submit" value="Submit" />
         </form>
-        <ul>
-          {this.state.listItems}
-        </ul>
+        <ToDoList items={this.state.listItems} />
       </div>
     );
   }
 }
 
-function ToDoList(props) {
+function App(props) {
   return (
     <div>
       <h1>To Do List</h1>
@@ -51,4 +99,4 @@ function ToDoList(props) {
   );
 }
 
-export default ToDoList;
+export default App;
