64a22831d08ecfd9f7be0ef762b0f0ed4f19d1ee
Gilad Shanan <giladshanan@gmail.com>
2017-11-02 10:26:41 -0500

    add form for adding items to the to do list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class ToDoForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      listItems: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A to do list item was submitted: ' + this.state.value);
+    event.preventDefault();
+    var newItem = <li key={this.state.listItems.length}>{this.state.value}</li>
+    this.setState({listItems: this.state.listItems.concat(newItem)});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Do this:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <ul>
+          {this.state.listItems}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+function ToDoList(props) {
+  return (
+    <div>
+      <h1>To Do List</h1>
+      <ToDoForm />
+    </div>
+  );
+}
+
+export default ToDoList;
